b16241a42685097b2cf78894a7232edb7589978f
TanyaMushonga <tanyaradzwatmushonga@gmail.com>
2024-03-07 02:08:36 +0200

    Add UserResponse model and update User and UserDetails models

--- WhatsappBot/Bot/models.py
+++ WhatsappBot/Bot/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
+
+class UserResponse(models.Model):
+    user_id = models.CharField(max_length=255)  # Assuming user_id is a string
+    user_name = models.CharField(max_length=255)
+    question = models.TextField()
+    response = models.TextField()
+
+    class Meta:
+        # To ensure each user can only have one response per questionfrom django.db import models
+        unique_together = ('user_id', 'question')
+
 # Create your models here.
+# Assuming the number is a string
+
+
+class User(models.Model):
+    # Assuming the number is a string
+    number = models.CharField(max_length=255)
+    question_index = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.number
+
+
+class UserDetails(models.Model):
+    full_name = models.CharField(max_length=200)
+    age = models.IntegerField()
+    location = models.CharField(max_length=200)
+    phone_number = models.CharField(max_length=20)
+    experience = models.TextField()
+    expenditure = models.TextField()
+    fishpond = models.BooleanField()
+
+
+class PersonalDetails(models.Model):
+    phone_number = models.CharField(max_length=20)
+    sender_name = models.CharField(max_length=100)
+    responses = models.TextField()
+
+    def __str__(self):
+        return self.sender_name
--- WhatsappBot/Bot/views.py
+++ WhatsappBot/Bot/views.py
@@ -1,27 +1,85 @@
+from django.db import IntegrityError
 from django.shortcuts import render
 import os
 from django.http import HttpResponse
 from twilio.rest import Client
 from django.views.decorators.csrf import csrf_exempt
+from .models import User, UserResponse
 
-# Create your views here. 
+# Create your views here.
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = 'AC42364285298c523c97e02ec136c7d5dc'
-auth_token = 'e0b8767477c99749954158e641ae9a3f'
+account_sid = 'AC057bcc46227f5deabe7c5ae30cc3c3d6'
+auth_token = 'd34a61046961debbd7cff870b82fef84'
 client = Client(account_sid, auth_token)
+
+questions = [
+    "What is your fullname?",
+    "What is you gender?",
+    "What is your age?",
+    "Where are you based?",
+    "Do you have an experience in fish farming if yes explain a brief",
+    "What is your capital expenditure?",
+    "Do you have a fishpond if yes how what is the size?",
+    "Which types of fish do you want farm?",
+]
+
 
 @csrf_exempt
 def bot(request):
     message = request.POST.get('Body')
     sender_name = request.POST.get('ProfileName')
     sender_number = request.POST.get('From')
+    user, created = User.objects.get_or_create(number=sender_number)
     print(f"sender:{sender_name} and the message is {message}")
-
-    if message == "hi":
+    existing_response = None
+    if created or message.lower() in ['hello', 'hi', 'hey']:
+        user.question_index = 0
+        user.save()
         client.messages.create(
             from_='whatsapp:+14155238886',
-            body=f'Hello, {sender_name}, how is it going',
-            to='whatsapp:+254721938869'
+            body=f'Hello {
+                sender_name}, how is it going, I am you fish farming chatbot. I am going to ask you a series of questions',
+            to='whatsapp:+263712389290'
         )
+    else:
+        existing_response = UserResponse.objects.filter(
+            user_id=sender_number, question=questions[user.question_index]).first()
+
+        if existing_response is None:
+            try:
+                UserResponse.objects.create(
+                    user_id=sender_number, user_name=sender_name, question=questions[user.question_index], response=message)
+                user.question_index += 1
+                user.save()
+            except IntegrityError:
+                options_message = "I have collected your information already. Here are some options you can choose from:\n" \
+                                  "1. How to get started with fish farming\n" \
+                                  "2. How to construct a fish pond\n" \
+                                  "3. How to design a feeding schedule for fish"
+                client.messages.create(
+                    from_='whatsapp:+14155238886',
+                    body=options_message,
+                    to='whatsapp:+263712389290'
+                )
+
+            if user.question_index < len(questions):
+                client.messages.create(
+                    from_='whatsapp:+14155238886',
+                    body=questions[user.question_index],
+                    to='whatsapp:+263712389290'
+                )
+            else:
+                client.messages.create(
+                    from_='whatsapp:+14155238886',
+                    body='Congrutulations you are now an official member of our fish farming community.',
+                    to='whatsapp:+263712389290'
+                )
+        else:
+            client.messages.create(
+                from_='whatsapp:+14155238886',
+                body=f'We have already collected information about please select the options below: {
+                    questions[user.question_index]}. Please wait for the next question',
+                to='whatsapp:+263712389290'
+            )
     return HttpResponse("Hello")
